3545e9f3bec56257b19b927974fcd3e316ddf6a0
Enetreal <wizzarddguru@gmail.com>
2024-05-07 09:33:23 -0700

    do

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""Function to query Reddit API"""
-
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Gets number of subscribers on a subreddit"""
-    link = 'https://www.reddit.com/r/{}/about.json'
-    usr = {'User-Agent': 'Linux David'}
-
-    try:
-        response = requests.get(link.format(subreddit),
-                                headers=usr,
-                                allow_redirects=False)
-        info = response.json()
-        subs = info['data']['subscribers']
-        return subs
-    except Exception:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
-"""Function to query Reddit API"""
-
-
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Returns ls of titles of the first ten hot posts"""
-    link = 'https://www.reddit.com/r/{}/hot.json'
-    usr = {'User-Agent': 'Linux David'}
-    heads = []
-
-    try:
-        response = requests.get(link.format(subreddit),
-                                headers=usr, params={'limit': 10},
-                                allow_redirects=False)
-
-        if response.status_code == 200:
-            info = response.json()
-            posts = info['data']['children']
-            for post in posts:
-                t = post['data']['title']
-                heads.append(t)
-        else:
-            return None
-
-    except Exception:
-        return None
-
-    return heads
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
